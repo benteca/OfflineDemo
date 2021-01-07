049ac01838e5d4f5009e0a167271a40485ec4ba9
Bente Cecilie Lio <bente-cecilie.lio@capgemini.com>
2021-01-07 18:30:53 +0100

    Added icons and changed manifest file

--- OfflineDemo/ClientApp/src/registerServiceWorker.js
+++ OfflineDemo/ClientApp/src/registerServiceWorker.js
@@ -19,7 +19,8 @@ const isLocalhost = Boolean(
 );
 
 export default function register () {
-  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    //if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    if ('serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location);
     if (publicUrl.origin !== window.location.origin) {
@@ -32,13 +33,13 @@ export default function register () {
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
-      if (isLocalhost) {
-        // This is running on localhost. Lets check if a service worker still exists or not.
-        checkValidServiceWorker(swUrl);
-      } else {
+      //if (isLocalhost) {
+      //  // This is running on localhost. Lets check if a service worker still exists or not.
+      //  checkValidServiceWorker(swUrl);
+      //} else {
         // Is not local host. Just register service worker
         registerValidSW(swUrl);
-      }
+      //}
     });
   }
 }
