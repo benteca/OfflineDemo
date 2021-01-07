dc021e1fe721a4d09a7c13d1fd338fd7970d23fc
Bente Cecilie Lio <bente-cecilie.lio@capgemini.com>
2021-01-07 23:17:29 +0100

    Added service worker. Able to install app.

---
+++ OfflineDemo/ClientApp/public/sw.js
@@ -0,0 +1,11 @@
+﻿self.addEventListener('install', evt => {
+    console.log('service worker has been installed.');
+});
+
+self.addEventListener('activate', evt => {
+    console.log('service worker has been activated.');
+});
+
+this.addEventListener('fetch', function (event) {
+    // it can be empty if you just want to get rid of that error
+});
